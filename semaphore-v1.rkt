3edbeb2e5f4e0c831676dac00eead1f47abf17d5
Luciano Martinez <lucianomartinez27@gmail.com>
2021-10-14 08:52:38 -0300

    first version of sempahore

---
+++ semaphore-v1.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname semaphore-v1) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; TrafficLight -> TrafficLight
+; yields the next state given current state s
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
+
+(check-expect (semaphore "red") (circle 10 "solid" "green"))
+(check-expect (semaphore "yellow") (circle 10 "solid" "red"))
+(check-expect (semaphore "green") (circle 10 "solid" "yellow"))
+              
+(define (semaphore color)
+  (circle 10 "solid" (traffic-light-next color) ))
+
+(define (main ws)
+  (big-bang ws
+    [to-draw semaphore]
+    [on-tick traffic-light-next 2]
+    ))
